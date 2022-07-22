2607f7e2bdb372a7e57078284d2f2d7799935b1d
chansenhyggedev <chansen.hyggedev@gmail.com>
2022-07-21 17:27:59 -0700

    added project links and repo

--- src/data/projects.js
+++ src/data/projects.js
@@ -16,7 +16,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/3119955/pexels-photo-3119955.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'Paleo live-edge chicharrones hoodie truffaut, typewriter humblebrag trust fund poutine tumeric actually vinyl selvage bicycle rights raw denim. Microdosing celiac hoodie, twee roof party la croix meggings edison bulb marfa tumblr.'
     },
@@ -37,7 +37,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/243908/pexels-photo-243908.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'Praxis tote bag kinfolk drinking vinegar, single-origin coffee raclette iceland vaporware gastropub truffaut cred tumblr. Pork belly 8-bit gentrify iPhone offal kinfolk.'
     },
@@ -58,7 +58,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/1374554/pexels-photo-1374554.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'Tousled ethical XOXO church-key blog, jean shorts pork belly. Mlkshk woke you probably havent heard of them +1 yr, pitchfork poutine ramps ugh iceland actually godard ethical paleo mumblecore.'
     },
@@ -79,7 +79,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/1185434/pexels-photo-1185434.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'Actually adaptogen tonx master cleanse, flannel gluten-free raw denim whatever iceland literally. Pug live-edge poke health goth, vape marfa plaid photo booth put a bird on it cred tote bag single-origin coffee jean shorts shabby chic.'
     },
@@ -100,7 +100,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/1601774/pexels-photo-1601774.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'Skateboard banh mi man braid iceland air plant distillery af helvetica hexagon umami sustainable tumblr. Taxidermy single-origin coffee crucifix cardigan tofu. '
     },
@@ -121,7 +121,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/3231775/pexels-photo-3231775.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'uccie tote bag prism bitters, portland etsy leggings. Pabst ramps cardigan, crucifix mumblecore slow-carb poutine quinoa seitan tofu chambray craft beer praxis vape viral.'
     },
@@ -142,7 +142,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/1996911/pexels-photo-1996911.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'Taxidermy single-origin coffee crucifix cardigan tofu. Yuccie tote bag prism bitters, portland etsy leggings. Pabst ramps cardigan, crucifix mumblecore slow-carb poutine quinoa seitan tofu chambray craft beer praxis vape viral. Literally pug whatever chambray bespoke. Hella tousled activated charcoal taiyaki godard post-ironic, kitsch forage salvia etsy.'
     },
@@ -163,7 +163,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/707408/pexels-photo-707408.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'Microdosing celiac hoodie, twee roof party la croix meggings edison bulb marfa tumblr. Direct trade street art tofu hashtag pabst blue bottle readymade pour-over squid retro four dollar toast tacos banh mi leggings succulents.'
     },
@@ -184,7 +184,7 @@ export const projects = [
             }
         ],
         liveURL: 'https://www.github.com/',
-        repoURL: 'https:/www.gitub.com/',
+        repoURL: 'https:/www.github.com/',
         cover: 'https://images.pexels.com/photos/942313/pexels-photo-942313.jpeg?auto=compress&cs=tinysrgb&w=1600',
         overview: 'Fixie fanny pack yr jean shorts, chambray gochujang leggings polaroid shaman four loko pitchfork cardigan venmo palo santo. Iceland tumeric humblebrag chicharrones street art intelligentsia +1 celiac DSA shaman banjo next level man bun fixie.'
     },
--- src/pages/ProjectView.js
+++ src/pages/ProjectView.js
@@ -12,12 +12,13 @@ import { motion } from "framer-motion";
         // exit={{ x: window.innerWidth}}
         className="w-full min-h-screen my-12">
         <div className="mxw-sm w-full flex items-center flex-col justify-center space-y-6 text-left sm:text-center my-12">
-            <p className="animate animate__animated animate__fadeInDown animate__fast font-semibold capitalize">{project?.category}</p>
-            <h2 className="animate animate__animated animate__fadeInDown animate__fast  text-4xl md:text-6xl font-bold capitalize">{project?.name}</h2>
+            <p className="animate animate__animated animate__fadeInDown animate__fast font-bold uppercase">{project?.category}</p>
+            <h2 className="animate animate__animated animate__fadeInDown animate__fast text-4xl md:text-6xl font-bold capitalize">{project?.name}</h2>
             <p className="animate animate__animated animate__fadeIn animate__slow text-xl font-light">{project?.excerpt}</p>
-            {/* <div className="animate animate__animated animate__fadeIn animate__slow flex items-center space-x-3">
-
-            </div> */}
+            <div className="flex items-center space-x-2">
+                <a target="_blank" rel="noreferrer" href={project?.liveURL} className="first-letter:text-2xl text-sm md:text-md first-letter:font-bold p-1 border-2 border-transparent hover:border-b-rose-600 duration-200">Visit Project</a>
+                <a target="_blank" rel="noreferrer" href={project?.repoURL} className="first-letter:text-2xl text-sm md:text-md first-letter:font-bold p-1 border-2 border-transparent hover:border-b-rose-600 duration-200">Github</a>
+            </div>
         </div>
         {/* <div className="w-full my-12 max-w-[200px] mx-auto h-3 bg-gray-200" /> */}
         {/* cover image */}
